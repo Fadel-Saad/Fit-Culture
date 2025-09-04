0b707d670e04733c98ab5e21e9e560e3b165b9f6
Fadel-Saad <fadelsaad.dev@gmail.com>
2025-09-05 00:26:35 +0300

    add featured icons

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,6 +2,7 @@ import ProductList from "@/components/shared/product/product-list";
 import { getLatestProducts, getFeaturedProducts } from "@/lib/actions/product.actions";
 import ProductCarousel from "@/components/shared/product/product-carousel";
 import ViewAllProductsButton from "@/components/view-all-products-button";
+import IconBoxes from "@/components/icon-boxes";
 
 async function Homepage() {
   const latestProducts = await getLatestProducts();
@@ -12,6 +13,7 @@ async function Homepage() {
       {featuredProducts.length > 0 && <ProductCarousel data={featuredProducts} />}
       <ProductList data={latestProducts} title="Newest Arrivals" limit={4} />
       <ViewAllProductsButton />
+      <IconBoxes />
     </>
   );
 }
